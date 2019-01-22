f1eb8805e92ebbee7a632e78f5bf25bb5d5cebba
gyoung410 <giyoung@bas.ac.uk>
2019-01-22 14:33:06 +0000

    Add dimensions variables

--- DataExtract_LEM-netCDF.py
+++ DataExtract_LEM-netCDF.py
@@ -11,7 +11,7 @@ import numpy as np
 import time 
 from datetime import datetime, timedelta 
 from netCDF4 import num2date, date2num 
-import os
+import matplotlib.pyplot as plt
 
 ##--------------------------------------------------------------------------
 ##--------------------------------------------------------------------------
@@ -99,7 +99,7 @@ timesec1 = (nc1['24']['TIMES'][:])/3600
 Z1 = nc1['24']['ZN'][:]
 X1 = nc1['24']['XN'][:]
 Y1 = nc1['24']['YN'][:]
-times=np.arange(1,23)
+times1=np.arange(1,23)
 
 ##--------------------------------------------------------------------------
 ##--------------------------------------------------------------------------
@@ -144,149 +144,63 @@ level = dataset.createDimension('level', np.size(icenum1,1))
 times = dataset.createVariable('time', np.float32, ('time',),fill_value='-9999') 
 levels = dataset.createVariable('level', np.int32, ('level',),fill_value='-9999') 
 
+
+###################################
+## Dimensions variables
+###################################
+#### Times
+times = dataset.createVariable('time', np.float64, ('Time_mid',),fill_value='-9999') 
+times.comment = 'Data dumps every 30mins'
+times.units = ['hours since 09:00:00 on 23-MAR-2013']
+times[:] = times1[:]
+
+#### Levels
+levels = dataset.createVariable('level', np.float64, ('Time_mid',),fill_value='-9999') 
+levels.long_name = 'Altitude'
+levels.comment = 'Levels spaced by 20m up to 1500m, then spaced by 50m between 1500m and 3000m'
+levels.units = 'm'
+levels[:] = Z1[:]
+
+###################################
 ###################################
 ## Create 2-d variables
 ###################################
-nisg = dataset.createVariable('nisg', np.float32, ('time','levels',),fill_value='-9999')
-nisg100 = dataset.createVariable('nisg100', np.float32, ('time','levels',),fill_value='-9999')
-nisg = dataset.createVariable('nisg', np.float32, ('time','levels',),fill_value='-9999')
+###################################
+#### Nisg
+nisg = dataset.createVariable('nisg', np.float32, ('time','level',),fill_value='-9999')
+nisg.long_name = 'total ice number concentration'
+nisg.comment = 'Sum of ice, snow, and graupel particles'
+nisg.units = 'm-3'
+nisg[:] = icenum1[:]
 
+#### Nisg100
+nisg100 = dataset.createVariable('nisg100', np.float32, ('time','level',),fill_value='-9999')
+nisg100.long_name = 'total ice number concentration greater than 100micron'
+nisg100.comment = 'Sum of ice, snow, and graupel particles of sizes greater than 100micron'
+nisg100.units = 'm-3'
+nisg100[:] = largeice1[:]
 
+#### Qliq
+qliq = dataset.createVariable('qliq', np.float32, ('time','level',),fill_value='-9999')
+qliq.long_name = 'cloud liquid mass mixing ratio'
+qliq.comment = 'Only cloud liquid field included; rain category is separate.'
+qliq.units = 'kg kg-1'
+qliq[:] = liqmass1[:]
 
-# ###################################
-# ## Create 4-d variables
-# ###################################
-# temperature = dataset.createVariable('temperature', np.float32, ('time','level','lat','lon'),fill_value='-9999') 
-# theta = dataset.createVariable('theta', np.float32, ('time','level','lat','lon'),fill_value='-9999') 
-# height = dataset.createVariable('height', np.float32, ('time','level','lat','lon'),fill_value='-9999') 
-# pressure = dataset.createVariable('pressure', np.float32, ('time','level','lat','lon'),fill_value='-9999') 
-# rho = dataset.createVariable('rho', np.float32, ('time','level','lat','lon'),fill_value='-9999') 
+#### Qvap
+qvap = dataset.createVariable('qvap', np.float32, ('time','level',),fill_value='-9999')
+qvap.long_name = 'water vapour mixing ratio'
+qvap.comment = ''
+qvap.units = 'kg kg-1'
+qvap[:] = watvap1[:]
 
-# qcloud = dataset.createVariable('qcloud', np.float32, ('time','level','lat','lon'),fill_value='-9999') 
-# nisg =  dataset.createVariable('nisg', np.float32, ('time','level','lat','lon'),fill_value='-9999') 
-# nisg100 =  dataset.createVariable('nisg100', np.float32, ('time','level','lat','lon'),fill_value='-9999') 
+#### Temperature
+temp = dataset.createVariable('temp', np.float32, ('time','level',),fill_value='-9999')
+temp.long_name = 'temperature'
+temp.comment = ''
+temp.units = 'K'
+temp[:] = tempK1[:]
 
-# ###################################
-# ## 3-d variables: standard names
-# # ###################################
-# swdnb.standard_name = 'surface_downwelling_shortwave_flux_in_air'
-# swdnbc.standard_name = 'surface_downwelling_shortwave_flux_in_air_assuming_clear_sky'
-# lwdnb.standard_name = 'surface_downwelling_longwave_flux_in_air'
-# lwdnbc.standard_name = 'surface_downwelling_longwave_flux_in_air_assuming_clear_sky'
-# swupb.standard_name = 'surface_upwelling_shortwave_flux_in_air'
-# swupbc.standard_name = 'surface_upwelling_shortwave_flux_in_air_assuming_clear_sky'
-# lwupb.standard_name = 'surface_upwelling_longwave_flux_in_air'
-# lwupbc.standard_name = 'surface_upwelling_longwave_flux_in_air_assuming_clear_sky'
-# seaice.standard_name = 'sea_ice_area_fraction'
-
-# # ###################################
-# # ## Create 4-d variables
-# # ###################################
-# temperature.standard_name = 'air_temperature'
-# theta.standard_name = 'air_potential_temperature'
-# height.standard_name = 'height'
-# pressure.standard_name = 'air_pressure'
-# rho.standard_name = 'air_density'
-
-# W.standard_name = 'vertical_wind_speed'
-# qvapor.standard_name = 'humidity_mixing_ratio'
-# qcloud.standard_name = 'cloud_liquid_water_mixing_ratio'
-# qrain.standard_name = 'rain_water_mixing_ratio'
-# qisg.standard_name = 'cloud_ice_mixing_ratio'
-# nisg.standard_name = 'number_concentration_of_ice_crystals_in_air'
-# nisg80.long_name = 'number_concentration_of_ice_crystals_larger_than_80micron_in_air'
-# nisg50.long_name = 'number_concentration_of_ice_crystals_smaller_than_50micron_in_air'
-
-# ###################################
-# ## Variable Attributes  
-# ###################################
-# times.units = 'hours since 2015-11-27 00:00:00'  
-# times.calendar = 'gregorian' 
-# levels.units = 'm' 
-# latitudes.units = 'degree_north'  
-# longitudes.units = 'degree_east'  
-
-# swdnb.units = 'W m-2'
-# swdnbc.units = 'W m-2'
-# lwdnb.units = 'W m-2'
-# lwdnbc.units = 'W m-2'
-# swupb.units = 'W m-2'
-# swupbc.units = 'W m-2'
-# lwupb.units = 'W m-2'
-# lwupbc.units = 'W m-2'
-# seaice.units = ''
-
-# temperature.units = 'K' 
-# theta.units = 'K' 
-# height.units = 'm'
-# pressure.units = 'Pa'
-# rho.units = 'kg m-3'
-
-# W.units = 'm s-1'
-# qvapor.units = 'kg kg-1'
-# qcloud.units = 'kg kg-1'
-# qrain.units = 'kg kg-1'
-# qisg.units = 'kg kg-1'
-# nisg.units = 'kg-1'
-# nisg80.units = 'kg-1'
-# nisg50.units = 'kg-1'
-
-
-# ###################################
-# ## Ice comments
-# ###################################
-# qisg.comment = 'Sum of ice + snow + graupel particle categories'
-# nisg.comment = 'Sum of ice + snow + graupel particle categories'
-# nisg80.comment = 'Sum of ice + snow + graupel particle categories. Particle sizes calculated online following the assumption that each hydrometeor class is represented by a Gamma distribution.'
-# nisg50.comment = 'Sum of ice + snow + graupel particle categories. Particle sizes calculated online following the assumption that each hydrometeor class is represented by a Gamma distribution.'
-
-# ###################################
-# ## Fill in times
-# ###################################
-# # dates = [] 
-# # for n in range(temp.shape[0]): 
-# # 	dates.append(datetime(2015, 11, 27) + n * timedelta(hours=0)) 
-# # 	times[:] = date2num(dates, units = times.units, calendar = times.calendar) 
-# # print 'time values (in units %s): ' % times.units + '\n', times[:] 
-
-# wrftime = nc1.variables['Times']
-# tim = np.zeros(np.size(data1['Tk'],0))
-# for i in range(np.size(data1['Tk'],0)):
-# 	str_times = wrftime[i][11:]
-# 	tim[i] = (np.int(str_times[0])*600 + np.int(str_times[1])*60 + np.int(str_times[3])*10)/float(60)
-
-# ###################################
-# ## Fill arrays
-# ###################################
-# times[:] = tim[:]
-# levels[:] = np.arange(0,np.size(data1['Z'],1))
-# latitudes[:,:,:] = data1['xlat'][:,:,:]
-# longitudes[:,:,:] = data1['xlon'][:,:,:]
-
-# swdnb[:,:,:] = data1['swdnb'][:,:,:]
-# swdnbc[:,:,:] = data1['swdnbc'][:,:,:]
-# lwdnb[:,:,:] = data1['lwdnb'][:,:,:]
-# lwdnbc[:,:,:] = data1['lwdnbc'][:,:,:]
-# swupb[:,:,:] = data1['swupb'][:,:,:]
-# swupbc[:,:,:] = data1['swupbc'][:,:,:]
-# lwupb[:,:,:] = data1['lwupb'][:,:,:]
-# lwupbc[:,:,:] = data1['lwupbc'][:,:,:]
-# seaice[:,:,:] = data1['seaice'][:,:,:]
-
-# temperature[:,:,:,:] = data1['Tk'][:,:,:,:]
-# theta[:,:,:,:] = data1['theta'][:,:,:,:]
-# height[:,:,:,:] = data1['Z'][:,:,:,:]
-# pressure[:,:,:,:] = data1['p'][:,:,:,:]
-# rho[:,:,:,:] = data1['rho'][:,:,:,:]
-
-# W[:,:,:,:] = data1['w'][:,:,:,:]
-# qvapor[:,:,:,:] = data1['qvap'][:,:,:,:]
-# qcloud[:,:,:,:] = data1['qcloud'][:,:,:,:]
-# qrain[:,:,:,:] = data1['qrain'][:,:,:,:]
-# qisg[:,:,:,:] = data1['qisg'][:,:,:,:]
-# nisg[:,:,:,:] = data1['qnisg'][:,:,:,:]
-# nisg80[:,:,:,:] = data1['nisg80'][:,:,:,:]
-# nisg50[:,:,:,:] = data1['nisg50'][:,:,:,:]
 
 ###################################
 ## Write out file
